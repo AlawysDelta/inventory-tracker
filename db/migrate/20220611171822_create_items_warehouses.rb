6f24f7641a6e59456b06371e5d8f869cc27bea03
Giovanni Borgh <gio96b@gmail.com>
2022-06-11 23:32:00 +0200

    Fixed Warehouse deletion issue

--- db/migrate/20220611171822_create_items_warehouses.rb
+++ db/migrate/20220611171822_create_items_warehouses.rb
@@ -1,8 +1,8 @@
 class CreateItemsWarehouses < ActiveRecord::Migration[7.0]
   def change
     create_table :items_warehouses do |t|
-      t.references :item, null: false, foreign_key: true
-      t.references :warehouse, null: false, foreign_key: true
+      t.references :item, null: false, foreign_key: false
+      t.references :warehouse, null: false, foreign_key: false
 
       t.timestamps
     end
